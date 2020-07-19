1b4732b277b04ac0e747bf624e984144360bfef7
iamtaegu <gmdals45@gmail.com>
2020-07-19 17:55:21 +0900

    macbook

---
+++ app.js
@@ -0,0 +1,40 @@
+var url = require('url');
+var express = require('express'); 
+var app = express(); 
+var router = express.Router();
+var fs = require('fs');
+
+var morgan = require('morgan');
+
+//app.use(morgan('combined'));
+
+router.get('/:id', function(request, response){
+
+    var _url = request.url;
+    var pathData = url.parse(_url, true).path;
+
+    console.log (_url);
+    console.log (pathData);
+
+    response.writeHead(200,{"Content-Type":"text/html"}); 
+    if(_url == '/' || _url == '/favicon.ico'){    
+        fs.createReadStream("index.html").pipe(response); 
+    } else {
+        fs.createReadStream('.'+pathData).pipe(response); 
+    }
+});
+
+
+app.all('*', function (request, response){
+
+    response.writeHead(200,{"Content-Type":"text/html"}); 
+    fs.createReadStream("index.html").pipe(response);
+
+});
+
+app.use('/', router);
+
+app.listen(80, function() {
+    console.log("Application Running at port 80");
+})
+
